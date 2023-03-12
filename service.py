e7c0f456c94b17494d72baaef1112634843bbdfb
Egor Datsishin <gok6@inbox.ru>
2023-03-12 15:41:09 +0300

    v8.1.3
    1. Исправлен вывод информации о пробеге, если нет информации об обслуживании. Криво, косо, но работает.

--- service.py
+++ service.py
@@ -4,21 +4,23 @@ from users import users_data, nl
 
 def check_mileage(name: str, new_mileage: int, user_id: int):
     coll = db_connect(user_id, param='service')
-    document = {'bike': users_data[f'{user_id}']['bike'],
+    bike = users_data[f'{user_id}']['bike']
+    document = {'bike': bike,
                 'name': name,
                 'mileage': new_mileage}
 
-    record = coll.find_one({'mileage': {'$exists': True}})
-
+    record = coll.find_one({'bike': {'$eq': bike},
+                            'last_chain_service': {'$exists': True},
+                            'last_drive_service': {'$exists': True}})
     if record:
         old_mileage = record['mileage']
-        last_chain_service = record['last_chain_service']
+        last_chain_service = record['last_drive_service']
         last_drive_service = record['last_drive_service']
 
         if new_mileage > old_mileage:
             coll.update_one(record, {'$set': {'mileage': new_mileage}})
             service_text = service(new_mileage, last_chain_service, last_drive_service)
-            return f'Пробег {name} увеличился на {new_mileage-old_mileage} км{nl}' \
+            return f'Пробег {name} увеличился на {new_mileage - old_mileage} км{nl}' \
                    f'Общий пробег составляет – {new_mileage} км{nl}{nl}' \
                    f'{service_text}'
 
@@ -28,6 +30,12 @@ def check_mileage(name: str, new_mileage: int, user_id: int):
             if service_text:
                 return f'{text}{nl}{nl}{service_text}'
             return f'{text}'
+
+    record = coll.find_one({'bike': {'$eq': bike}})
+    text = f'Пробег {name} составляет – {new_mileage} км'
+    if record:
+        return f'{text}{nl}{nl}' \
+               f'Нет данных по обслуживанию велосипеда, добавьте информацию'
 
     coll.insert_one(document)
     text = f'Пробег {name} составляет – {new_mileage} км'
