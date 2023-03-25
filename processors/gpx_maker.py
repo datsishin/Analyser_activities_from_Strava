9e5c20e2f49553d044a02c333a95509bce8b08cc
Valya <valya_belezyak@mail.ru>
2023-03-25 23:49:42 +0300

    v10.8.1
    1. Небольшие исправления для корректной работы с numpy

--- processors/gpx_maker.py
+++ processors/gpx_maker.py
@@ -39,7 +39,7 @@ def processing_data(response_hr: list, response_power: list, user_id: int):
             data_without_zero_and_none = [i for i in data if i is not None and i != 0]
             hr_data = np.asarray(data_without_zero_and_none)
             break
-        # hr_data = []
+        hr_data = np.asarray()
 
     for i in range(0, len(response_power)):
         if response_power[i]['type'] == 'watts':
@@ -47,7 +47,7 @@ def processing_data(response_hr: list, response_power: list, user_id: int):
             data_without_zero_and_none = [i for i in data if i is not None and i != 0]
             power_data = np.asarray(data_without_zero_and_none)
             break
-        # power_data = []
+        power_data = np.asarray()
 
     # make_power_by_hr_graph()
     return data_checker(user_id)
