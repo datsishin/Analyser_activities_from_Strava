3d272557fe3fbf74f3be63b0d3aab6b11625139c
valentina_belezak <valya_belezyak@mail.ru>
2024-12-03 22:01:01 +0300

    hotfix/1: поменял пульсовые зоны под рекомендации с trisystems

--- processors/hr_analyser.py
+++ processors/hr_analyser.py
@@ -6,28 +6,42 @@ def get_hr_statistics(hr_data: list, user_id: int):
     threshold = int(users_data[f'{user_id}']['threshold'])
     hr_max = int(users_data[f'{user_id}']['hr_max'])
 
-    zone_1_by_hr = [threshold * 0.65, threshold * 0.81]
-    zone_2_by_hr = [threshold * 0.81, threshold * 0.88]
-    zone_3_by_hr = [threshold * 0.88, threshold * 0.93]
-    zone_4_by_hr = [threshold * 0.93, threshold * 1.00]
-    zone_5_by_hr = [threshold * 1.00, threshold * 1.02]
-    zone_6_by_hr = [threshold * 1.02, threshold * 1.05]
-    zone_7_by_hr = [threshold * 1.05, hr_max]
+    # zone_1_by_hr = [threshold * 0.65, threshold * 0.81]
+    # zone_2_by_hr = [threshold * 0.81, threshold * 0.88]
+    # zone_3_by_hr = [threshold * 0.88, threshold * 0.93]
+    # zone_4_by_hr = [threshold * 0.93, threshold * 1.00]
+    # zone_5_by_hr = [threshold * 1.00, threshold * 1.02]
+    # zone_6_by_hr = [threshold * 1.02, threshold * 1.05]
+    # zone_7_by_hr = [threshold * 1.05, hr_max]
+
+    zone_1_by_hr = [threshold * 0.60, threshold * 0.75]
+    zone_2_by_hr = [threshold * 0.76, threshold * 0.84]
+    zone_3_by_hr = [threshold * 0.85, threshold * 0.94]
+    zone_4_by_hr = [threshold * 0.95, threshold * 1.01]
+    zone_5_by_hr = [threshold * 1.02, hr_max]
+
+    # list_of_zone = {
+    #     'zone_1_by_hr': f'Восстановление {nl}{int(zone_1_by_hr[0])} – {int(zone_1_by_hr[1])}',
+    #     'zone_2_by_hr': f'Экстенсивная {nl} выносливость {nl}{int(zone_2_by_hr[0])} – {int(zone_2_by_hr[1])}',
+    #     'zone_3_by_hr': f'Интенсивная {nl} выносливость {nl}{int(zone_3_by_hr[0])} – {int(zone_3_by_hr[1])}',
+    #     'zone_4_by_hr': f'СубПАНО {nl}{int(zone_4_by_hr[0])} – {int(zone_4_by_hr[1])}',
+    #     'zone_5_by_hr': f'СверхПАНО {nl}{int(zone_5_by_hr[0])} – {int(zone_5_by_hr[1])}',
+    #     'zone_6_by_hr': f'Аэробная {nl}{int(zone_6_by_hr[0])} – {int(zone_6_by_hr[1])}',
+    #     'zone_7_by_hr': f'Анаэробная {nl}{int(zone_7_by_hr[0])} – {int(zone_7_by_hr[1])}',
+    # }
 
     list_of_zone = {
         'zone_1_by_hr': f'Восстановление {nl}{int(zone_1_by_hr[0])} – {int(zone_1_by_hr[1])}',
-        'zone_2_by_hr': f'Экстенсивная {nl} выносливость {nl}{int(zone_2_by_hr[0])} – {int(zone_2_by_hr[1])}',
-        'zone_3_by_hr': f'Интенсивная {nl} выносливость {nl}{int(zone_3_by_hr[0])} – {int(zone_3_by_hr[1])}',
-        'zone_4_by_hr': f'СубПАНО {nl}{int(zone_4_by_hr[0])} – {int(zone_4_by_hr[1])}',
-        'zone_5_by_hr': f'СверхПАНО {nl}{int(zone_5_by_hr[0])} – {int(zone_5_by_hr[1])}',
-        'zone_6_by_hr': f'Аэробная {nl}{int(zone_6_by_hr[0])} – {int(zone_6_by_hr[1])}',
-        'zone_7_by_hr': f'Анаэробная {nl}{int(zone_7_by_hr[0])} – {int(zone_7_by_hr[1])}',
+        'zone_2_by_hr': f'Выносливость {nl}{int(zone_2_by_hr[0])} – {int(zone_2_by_hr[1])}',
+        'zone_3_by_hr': f'Темповая {nl}{int(zone_3_by_hr[0])} – {int(zone_3_by_hr[1])}',
+        'zone_4_by_hr': f'ПАНО {nl}{int(zone_4_by_hr[0])} – {int(zone_4_by_hr[1])}',
+        'zone_5_by_hr': f'VO2 Max {nl}{int(zone_5_by_hr[0])} – ∞',
     }
 
     dicts_of_zones = {}
 
     i = 1
-    while i <= 7:
+    while i <= len(list_of_zone):
         keyword = f'time_in_zone_' + str(i) + '_by_hr'
         value = 0
         dicts_of_zones[keyword] = value
@@ -47,7 +61,7 @@ def get_hr_statistics(hr_data: list, user_id: int):
                     finish_range = eval(f'zone_' + str(i) + '_by_hr')[1]
                     key = f'time_in_zone_' + str(i) + '_by_hr'
 
-                    if start_range < hr < finish_range:
+                    if start_range <= hr <= finish_range:
                         new_value = dicts_of_zones[key] + 1
                         dicts_of_zones.update({key: new_value})
 
--- processors/power_analyser.py
+++ processors/power_analyser.py
@@ -5,28 +5,34 @@ from users import users_data, nl
 def get_power_statistics(power_data: list, user_id: int):
     ftp = int(users_data[f'{user_id}']['ftp'])
 
-    zone_1_by_power = [0, ftp * 0.56]
-    zone_2_by_power = [ftp * 0.56, ftp * 0.75]
-    zone_3_by_power = [ftp * 0.75, ftp * 0.90]
-    zone_4_by_power = [ftp * 0.90, ftp * 1.05]
-    zone_5_by_power = [ftp * 1.05, ftp * 1.20]
-    zone_6_by_power = [ftp * 1.21, ftp * 1.50]
-    zone_7_by_power = [ftp * 1.50, 5000]
+    # zone_1_by_power = [0, ftp * 0.56]
+    # zone_2_by_power = [ftp * 0.56, ftp * 0.75]
+    # zone_3_by_power = [ftp * 0.75, ftp * 0.90]
+    # zone_4_by_power = [ftp * 0.90, ftp * 1.05]
+    # zone_5_by_power = [ftp * 1.05, ftp * 1.20]
+    # zone_6_by_power = [ftp * 1.21, ftp * 1.50]
+    # zone_7_by_power = [ftp * 1.50, 5000]
+
+    zone_1_by_power = [ftp * 0.5, ftp * 0.65]
+    zone_2_by_power = [ftp * 0.66, ftp * 0.75]
+    zone_3_by_power = [ftp * 0.76, ftp * 0.92]
+    zone_4_by_power = [ftp * 0.93, ftp * 1.00]
+    zone_5_by_power = [ftp * 1.01, 2000]
 
     list_of_zone = {
         'zone_1_by_power': f'Восстановление {nl}{int(zone_1_by_power[0])} – {int(zone_1_by_power[1])}',
-        'zone_2_by_power': f'Экстенсивная {nl} выносливость {nl}{int(zone_2_by_power[0])} – {int(zone_2_by_power[1])}',
-        'zone_3_by_power': f'Интенсивная {nl} выносливость {nl}{int(zone_3_by_power[0])} – {int(zone_3_by_power[1])}',
+        'zone_2_by_power': f'Выносливость {nl}{int(zone_2_by_power[0])} – {int(zone_2_by_power[1])}',
+        'zone_3_by_power': f'Темповая {nl}{int(zone_3_by_power[0])} – {int(zone_3_by_power[1])}',
         'zone_4_by_power': f'FTP {nl}{int(zone_4_by_power[0])} – {int(zone_4_by_power[1])}',
-        'zone_5_by_power': f'Аэробная {nl}{int(zone_5_by_power[0])} – {int(zone_5_by_power[1])}',
-        'zone_6_by_power': f'Анаэробная {nl}{int(zone_6_by_power[0])} – {int(zone_6_by_power[1])}',
-        'zone_7_by_power': f'Мощность {nl}{int(zone_7_by_power[0])} – ∞',
+        'zone_5_by_power': f'Анаэробная {nl}{int(zone_5_by_power[0])} – ∞',
+        # 'zone_6_by_power': f'Анаэробная {nl}{int(zone_6_by_power[0])} – {int(zone_6_by_power[1])}',
+        # 'zone_7_by_power': f'Мощность {nl}{int(zone_7_by_power[0])} – ∞',
     }
 
     dicts_of_zones = {}
 
     i = 1
-    while i <= 7:
+    while i <= len(list_of_zone):
         keyword = f'time_in_zone_' + str(i) + '_by_power'
         value = 0
         dicts_of_zones[keyword] = value
@@ -45,7 +51,7 @@ def get_power_statistics(power_data: list, user_id: int):
                 finish_range = eval(f'zone_' + str(i) + '_by_power')[1]
                 key = f'time_in_zone_' + str(i) + '_by_power'
 
-                if start_range < power < finish_range:
+                if start_range <= power <= finish_range:
                     new_value = dicts_of_zones[key] + 1
                     dicts_of_zones.update({key: new_value})
 
